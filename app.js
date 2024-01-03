c615bd64aaee9aec49e4af35c00d265c3c806243
Anlimah <y.m.ratty7@gmail.com>
2024-01-03 06:39:21 +0000

    Testing setup

--- app.js
+++ app.js
@@ -2,15 +2,10 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
 
 app.use(express.json());
 
-app.post('/webhook', (req, res) => {
+app.post('/webhook', express.json({type: 'application/json'}), (req, res) => {
   const event = req.headers['x-github-event'];
   const payload = req.body;
 
@@ -18,12 +13,18 @@ app.post('/webhook', (req, res) => {
 
   // Handle different GitHub events here
   switch (event) {
+    case 'ping':
+        handlePingEvent(payload);
+        break;
+
     case 'push':
-      handlePushEvent(payload);
-      break;
+        handlePushEvent(payload);
+        break;
+
     case 'issues':
-      handleIssuesEvent(payload);
-      break;
+        handleIssuesEvent(payload);
+        break;
+        
     // Add more cases for other events as needed
     default:
       break;
@@ -31,6 +32,11 @@ app.post('/webhook', (req, res) => {
 
   res.status(200).send('Webhook received successfully');
 });
+
+function handlePingEvent(payload) {
+  // Handle issues event (e.g., notify users about new issues)
+  console.log('Issues event received:', payload);
+}
 
 function handlePushEvent(payload) {
   // Handle push event (e.g., notify users about new commits)
@@ -41,3 +47,10 @@ function handleIssuesEvent(payload) {
   // Handle issues event (e.g., notify users about new issues)
   console.log('Issues event received:', payload);
 }
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
